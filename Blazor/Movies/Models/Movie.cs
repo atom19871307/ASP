af5ff72bc703b344ba4b740989653333881c9445
Atom Mayilyan <mayilyan.atom@mail.ru>
2026-09-12 21:08:36 +0300

    Lesson 20 - DataAnnotations

--- Blazor/Movies/Models/Movie.cs
+++ Blazor/Movies/Models/Movie.cs
@@ -1,9 +1,16 @@
-﻿namespace Movies.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.Models
 {
     public class Movie
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+
+        [Required]
+        //[StringLength(50, MinimumLength = 2)]
+        [MinLength(2, ErrorMessage = "Слишком мало"), MaxLength(50, ErrorMessage = "Слишком много")] public string Title { get; set; }
+
+        [RangeAttribute(typeof(DateOnly), "1888-10-14", "2128-12-31")]
         public DateOnly ReleaseDate { get; set; }
         public string? Genre { get; set; }
         public string? URL { get; set; }
