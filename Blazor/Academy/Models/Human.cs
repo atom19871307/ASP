2c2199c9ae6aced432055239e7638a37851d0370
Atom Mayilyan <mayilyan.atom@mail.ru>
2026-09-24 22:30:35 +0300

    Lesson 29-Student model DONE, TODO: Scaffold Student.

---
+++ Blazor/Academy/Models/Human.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy.Models
+{
+    public class Human
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        [RegularExpression("^[A-ZА-Я][a-zа-я]*$", ErrorMessage = "Фамилия содержит недопустимые символы")]
+        public string last_name { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        [RegularExpression("^[A-ZА-Я][a-zа-я]*$", ErrorMessage = "Имя содержит недопустимые символы")]
+        public string first_name { get; set; }
+
+        [StringLength(50, MinimumLength = 2)]
+        [RegularExpression("^[A-ZА-Я][a-zа-я]*$", ErrorMessage = "Отчество содержит недопустимые символы")]
+        public string? middle_name { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [RangeAttribute(typeof(DateOnly), "1950-01-01", "2020-01-01")]
+        public DateOnly birth_date { get; set; }
+
+        public string? email { get; set; }
+        public string? phone { get; set; }
+        public byte[]? photo { get; set; }
+
+        //Calculated properties:
+        public string FullName
+        {
+            get => $"{last_name} {first_name} {middle_name}";
+        }
+        public int Age
+        {
+            get => (int)((DateOnly.FromDateTime(DateTime.Now).DayNumber - birth_date.DayNumber) / 365.25);
+        }
+    }
+}
